bccb45992142aeed7e70cf0587fafaf7d6aba5e1
161360238 <1782242508@qq.com>
2018-11-25 19:42:51 +0800

    加入异常处理

--- src/cn/wj/exception/CustomExceptionResolver.java
+++ src/cn/wj/exception/CustomExceptionResolver.java
@@ -10,47 +10,39 @@ import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * 
- * <p>Title: CustomExceptionResolver</p>
- * <p>Description: 鑷畾涔夊紓甯稿鐞嗗櫒</p>
- * <p>Company: www.itcast.com</p> 
- * @author	浼犳櫤.鐕曢潚
- * @date	2015-3-22涓嬪崍2:57:42
- * @version 1.0
- */
+
 public class CustomExceptionResolver implements HandlerExceptionResolver  {
 
-	//鍓嶇鎺у埗鍣―ispatcherServlet鍦ㄨ繘琛孒andlerMapping銆佽皟鐢℉andlerAdapter鎵цHandler杩囩▼涓紝濡傛灉閬囧埌寮傚父灏变細鎵ц姝ゆ柟娉�
-	//handler鏈�粓瑕佹墽琛岀殑Handler锛屽畠鐨勭湡瀹炶韩浠芥槸HandlerMethod
-	//Exception ex灏辨槸鎺ユ敹鍒板紓甯镐俊鎭�
+	//前端控制器DispatcherServlet在进行HandlerMapping、调用HandlerAdapter执行Handler过程中，如果遇到异常就会执行此方法
+	//handler最终要执行的Handler，它的真实身份是HandlerMethod
+	//Exception ex就是接收到异常信息
 	@Override
 	public ModelAndView resolveException(HttpServletRequest request,
 			HttpServletResponse response, Object handler, Exception ex) {
-		//杈撳嚭寮傚父
+		//输出异常
 		ex.printStackTrace();
 		
-		//缁熶竴寮傚父澶勭悊浠ｇ爜
-		//閽堝绯荤粺鑷畾涔夌殑CustomException寮傚父锛屽氨鍙互鐩存帴浠庡紓甯哥被涓幏鍙栧紓甯镐俊鎭紝灏嗗紓甯稿鐞嗗湪閿欒椤甸潰灞曠ず
-		//寮傚父淇℃伅
+		//统一异常处理代码
+		//针对系统自定义的CustomException异常，就可以直接从异常类中获取异常信息，将异常处理在错误页面展示
+		//异常信息
 		String message = null;
 		CustomException customException = null;
-		//濡傛灉ex鏄郴缁�鑷畾涔夌殑寮傚父锛岀洿鎺ュ彇鍑哄紓甯镐俊鎭�
+		//如果ex是系统 自定义的异常，直接取出异常信息
 		if(ex instanceof CustomException){
 			customException = (CustomException)ex;
 		}else{
-			//閽堝闈濩ustomException寮傚父锛屽杩欑被閲嶆柊鏋勯�鎴愪竴涓狢ustomException锛屽紓甯镐俊鎭负鈥滄湭鐭ラ敊璇�
-			customException = new CustomException("鏈煡閿欒");
+			//针对非CustomException异常，对这类重新构造成一个CustomException，异常信息为“未知错误”
+			customException = new CustomException("未知错误");
 		}
 		
-		//閿欒 淇℃伅
+		//错误 信息
 		message = customException.getMessage();
 		
 		request.setAttribute("message", message);
 
 		
 		try {
-			//杞悜鍒伴敊璇�椤甸潰
+			//转向到错误 页面
 			request.getRequestDispatcher("/WEB-INF/jsp/error.jsp").forward(request, response);
 		} catch (ServletException e) {
 			// TODO Auto-generated catch block
